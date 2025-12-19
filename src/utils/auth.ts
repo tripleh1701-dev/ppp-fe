b12514a39f65dacee46dbdd5e246a75faf85f119
tripleh1701-dev <tripleh1701@gmail.com>
2025-12-19 17:27:57 +0530

    Update auth.ts

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -181,7 +181,7 @@ export const completePasswordChallenge = async (
 
     try {
         const response = await fetch(
-            `${API_BASE_URL}${API_VERSION}/auth/complete-password-challenge`,
+            `${API_BASE_URL}${API_VERSION}/auth/complete-new-password-challenge`,
             {
                 method: 'POST',
                 headers: {
